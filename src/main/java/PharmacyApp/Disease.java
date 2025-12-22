647157f3a5af120929ab8dec8d15a34c9c264d1c
tihiyden <D4P783uD3P@yandex.ru>
2025-12-22 21:38:05 +0300

    final app version

--- src/main/java/PharmacyApp/Disease.java
+++ src/main/java/PharmacyApp/Disease.java
@@ -1,43 +1,64 @@
 package PharmacyApp;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+@Entity
+@Table(name = "disease")
 public class Disease {
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "disease_id")
+    private Integer id;
+
+    @Column(name = "disease_name", nullable = false)
     private String name;
-    private List<Medicine> treatment;
-    private String ICD_id;
 
+    @Column(name = "icd_code")
+    private String icdCode;
 
-    public void SetName(String _name) {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "disease_medicine",
+        joinColumns = @JoinColumn(name = "disease_id"),
+        inverseJoinColumns = @JoinColumn(name = "medicine_id")
+    )
+    private List<Medicine> treatments = new ArrayList<>();
 
+    public Disease() {}
+
+    public Disease(String name, String icdCode) {
+        this.name = name;
+        this.icdCode = icdCode;
     }
 
-    public String GetName() {
-        return name;
+    public Integer getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getIcdCode() { return icdCode; }
+    public void setIcdCode(String icdCode) { this.icdCode = icdCode; }
+
+    public List<Medicine> getTreatments() { return treatments; }
+    public void addTreatment(Medicine m) { treatments.add(m); }
+    public void removeTreatment(Medicine m) { treatments.remove(m); }
+    @Override
+    public String toString() {
+    	return name;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Disease)) return false;
+        Disease disease = (Disease) o;
+        return Objects.equals(id, disease.id);
     }
 
-    public void ChangeICD(String _id) {
-
-    }
-
-    public String GetICD() {
-        return ICD_id;
-    }
-
-
-    public void AddTreatment(Medicine medicine) {
-
-    }
-
-    public void RemoveTreatment(Medicine medicine) {
-
-    }
-
-    public List<Medicine> GetTreatments() {
-        return treatment;
-    }
-
-    public void RemoveAllTreatments() {
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/PharmacyApp/Medicine.java
+++ src/main/java/PharmacyApp/Medicine.java
@@ -1,45 +1,81 @@
 package PharmacyApp;
 
 import jakarta.persistence.*;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
-@Table(name = "medicine", schema = "test")
+@Table(name = "medicine")
 public class Medicine {
-	
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "medicine_id")
-	private Integer id;
-    
-    @Column(name = "medicine_name")
+    private Integer id;
+
+    @Column(name = "medicine_name", nullable = false)
     private String name;
-    
+
     @Column(name = "medicine_quantity")
     private Integer quantity;
-    
+
     @Column(name = "medicine_cost")
     private Float cost;
-    
-    
-    //private List<Disease> treats;
 
-    
+
+    @ManyToMany(mappedBy = "treatments", fetch = FetchType.EAGER)
+    private List<Disease> diseases = new ArrayList<>();
+
+    public Medicine() {}
+
+    public Medicine(String name, Integer quantity, Float cost) {
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+    }
+
     public Integer getId() { return id; }
-    public void setIdm(Integer _id) { this.id = _id; }
+
     public String getName() { return name; }
-    public void setName(String _name) { this.name = _name; }
+    public void setName(String name) { this.name = name; }
+
     public Integer getQuantity() { return quantity; }
-    public void setQuantity(Integer _quantity) { this.quantity = _quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+
     public Float getCost() { return cost; }
-    public void setCost(Float _cost) { this.cost = _cost; }
+    public void setCost(Float cost) { this.cost = cost; }
 
 
-    //public void AddDisease(Disease disease) {}
+    public List<Disease> getDiseases() { return diseases; }
 
-    //public void RemoveDisease(Disease disease) {}
+    public void addDisease(Disease d) { diseases.add(d); }
 
-    //public List<Disease> GetTreats() {return treats;}
+    public void removeDisease(Disease d) { diseases.remove(d); }
 
-    //public void RemoveAllDiseases() {}
+    @Override
+    public String toString() {
+    	/*
+        return "Medicine{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", cost=" + cost +
+                '}';
+        */
+    	return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Medicine)) return false;
+        Medicine medicine = (Medicine) o;
+        return Objects.equals(id, medicine.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
